fe2cc0c6eadb14abd5477e58319b7207becba863
= <AndrewWiggin@example.com>
2022-05-02 09:10:19 -0700

    TableRow

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 
+const TableRow = (props) => {
+  return(
+    <tr>
+      {props.v.map(c => (
+      <td><Greet char={c}/></td>
+      ))}
+    </tr>)
+}
+
 function App() {
-  const animals = ["Dog", "Bird", "Cat", "Mouse", "Horse"];
+  const animals = [["Dog", "Bird", "Cat", "Mouse", "Horse"],
+  ["Chicken","Raven","Possum","Monkey","Shrimp"]];
 
   return (
-    <ul>
-      {animals.map(animal => (
-        <li><Greet char={animal}/></li>
+    <div>
+    <tr>
+      {animals[0].map(animal => (
+        <td><Greet char={animal}/></td>
       ))}
-    </ul>
+    </tr>
+    <TableRow v={animals[1]}/>
+    </div>
   );
 };
 
