1712e5cc41dce8488473555a27b7f48a6bc6b8de
= <AndrewWiggin@example.com>
2022-05-02 17:07:16 -0700

    row foor loop

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet'
 
+const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+const alphabet = alpha.map((x) => String.fromCharCode(x));
+console.log(alphabet);
+
+function getRandomChar() {
+  return String.fromCharCode(Math.floor(Math.random() * 26 + 65));
+}
+
 const TableRow = (props) => {
-  return(
-    <tr>
-      {props.v.map(c => (
-      <td><Greet char={c}/></td>
-      ))}
-    </tr>)
+  let content = [];
+  for (let i = 0; i < 5; i++){
+    content.push(<td><Greet char={getRandomChar()}/></td>)
+  }
+  return(<tr>{content}</tr>)
 }
 
 const Table = (props) => {
